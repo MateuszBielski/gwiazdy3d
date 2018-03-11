d035d8cd4958c22ff7078673e2fe3cca420bd114
Mateusz Bielski <mateo_bass@bialan.pl>
2018-03-11 15:08:41 +0100

    określono autora

--- OdczytKolorowBMP.h
+++ OdczytKolorowBMP.h
@@ -1,12 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
 /* 
  * File:   OdczytKolorowBMP.h
- * Author: BCO
+ * Author: Mateusz Bielski
  *
  * Created on 4 maja 2016, 07:46
  */
--- OdczytWierzcholkow.h
+++ OdczytWierzcholkow.h
@@ -1,12 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /* 
  * File:   OdczytWierzcholkow.h
- * Author: BCO
+ * Author: Mateusz Bielski
  *
  * Created on 19 lutego 2016, 07:56
  */

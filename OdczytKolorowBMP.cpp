a2cc05db1771561f99caac7001dc162a2b02806c
Mateusz Bielski <mateo_bass@bialan.pl>
2018-03-11 15:22:04 +0100

    ustawiono autora w cpp

--- OdczytKolorowBMP.cpp
+++ OdczytKolorowBMP.cpp
@@ -1,12 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /* 
  * File:   OdczytKolorowBMP.cpp
- * Author: BCO
+ * Author: Mateusz Bielski
  * 
  * Created on 4 maja 2016, 07:46
  */
--- RysujGwiazdy.cpp
+++ RysujGwiazdy.cpp
@@ -1,12 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /* 
  * File:   RysujGwiazdy.cpp
- * Author: BCO
+ * Author: Mateusz Bielski
  * 
  * Created on 10 kwietnia 2016, 13:15
  */
